8e282ede8d7f79cd087566249ef025e7543217a4
Yong Bin <yongb@usi.ch>
2023-10-24 20:30:52 +0000

    code.sh: open directory

--- code.sh
+++ code.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 env_dir=$(readlink -f $(dirname "$0"))/environment
 mkdir -p "$env_dir/code/extensions" "$env_dir/code/data"
-/usr/local/share/code-oss/code-oss --extensions-dir "$env_dir/code/extensions" --disable-gpu --ms-enable-electron-run-as-node --disable-software-rasterizer
+/usr/local/share/code-oss/code-oss --extensions-dir "$env_dir/code/extensions" --disable-gpu --ms-enable-electron-run-as-node .
 # --user-data-dir "$env_dir/code/data"
