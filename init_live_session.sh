6798a2d7d3517caabca1248c0da2717c34f34c35
Yong Bin <yongb@usi.ch>
2023-10-27 18:53:50 +0000

    live session: add support to use session under hood

--- init_live_session.sh
+++ init_live_session.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#sudo pkg -c $(dirname "$0")/pkg_environment/ install texlive-full vscode
+export SOCKS5_PROXY="10.0.2.1:11371"
+git config --global http.proxy 'socks5://10.0.2.1:11371'
+#sudo -E pkg -c $(dirname "$0")/pkg_environment/ install texlive-full vscode
 script_dir=$(readlink -f $(dirname "$0"))
 echo $script_dir
 sudo service avahi-daemon stop
@@ -27,7 +29,7 @@ set skip on lo0
 set block-policy drop
 block in quick inet6 all
 block out quick inet6 all
-pass out quick proto tcp to any port {443, 80, 53} keep state
+pass out quick proto tcp to any port {11371, 443, 80, 53} keep state
 pass out log quick proto udp to any port {53} keep state
 pass out log quick proto udp from any port 67 to any port 68 keep state
 block log  quick all
@@ -53,6 +55,14 @@ sudo tee -a /etc/hosts <<EOF
 0.0.0.0 redirector.gvt1.com
 0.0.0.0 openvsxorg.blob.core.windows.net
 EOF
+
+if ! grep socks5 /usr/local/etc/pkg.conf; then
+sudo tee -a /usr/local/etc/pkg.conf <<EOF
+pkg_env: {
+  socks5_proxy: "10.0.2.1:11371"
+}
+EOF
+fi
 
 if ! ifconfig em0|grep active; then
   sudo ifconfig em0 link random
@@ -80,11 +90,11 @@ if ! [ -f /tmp/ca-root-nss.crt ]; then
   sudo certctl rehash
 fi
 
-if sudo pkg upgrade -y firefox; then
-  sudo pkg install -y texlive-base vscode cmake-core
+if sudo -E pkg upgrade -y firefox; then
+  sudo -E pkg install -y texlive-base vscode cmake-core
   sudo mv /usr/local/share/texmf-dist /usr/local/share/texmf-dist-old
   sudo ln -s $script_dir/environment/usr/local/share/texmf-dist /usr/local/share/texmf-dist
-  sudo pkg -o RUN_SCRIPTS=false -o INSTALL_AS_USER=true add download/fake-texmf.tar
-  sudo pkg install -y latex-mk
-  sudo pkg -o INSTALL_AS_USER=true install -y tex-dvipsk
+  sudo -E pkg -o RUN_SCRIPTS=false -o INSTALL_AS_USER=true add download/fake-texmf.tar
+  sudo -E pkg install -y latex-mk
+  sudo -E pkg -o INSTALL_AS_USER=true install -y tex-dvipsk
 fi
