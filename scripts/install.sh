5d7fc9b7e0448a78547b72284fc38734a0fef919
Yong Bin <yongb@usi.ch>
2023-10-30 15:25:42 -0900

    scripts: 1. add nftables support 2. add prefix option in install.sh 3. fix bugs

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,77 +10,90 @@ elif [ $machine = "Linux" ]; then
 fi
 
 harden_only=0
+prefix=""
 for arg in "$@"; do
-  if [ $arg = "harden_only" ]; then
-    harden_only=1
-  fi
+  case $arg in 
+    harden_only) harden_only=1;;
+    prefix=*) prefix=$(echo $arg|sed "s/.*=//g");;
+  esac
 done
 
-if [ $harden_only == 0 ]; then
-  sudocpcontent ./rc.local ~/m2/etc/
-  sudocpcontent ./danted.conf ~/m2/etc/
+echo $harden_only $prefix
+#exit
 
-  sudo rm ~/m2/etc/systemd/system/multi-user.target.wants/userconfig.service
-  sudo rm ~/m2/etc/systemd/system/getty.target.wants/getty@tty1.service
+if [ $harden_only -eq 1 ]; then
+  sudo touch $prefix/var/hood_harden_only
+  if ! grep disable-bt $prefix/boot/firmware/config.txt; then
+    sudo tee -a $prefix/boot/firmware/config.txt <<EOF
+dtoverlay=disable-bt
+dtoverlay=disable-wifi
+EOF
+  fi
+else
+  sudo cp -r boot/* $prefix/boot/firmware/
+  sudocpcontent ./modules $prefix/etc/
+  sudocpcontent ./rc.local $prefix/etc/
+  sudocpcontent ./danted.conf $prefix/etc/
+  sudocpcontent ./00-firewall.conf $prefix/etc/rsyslog.d/
 
-  sudo cp system-connections/* ~/m2/etc/NetworkManager/system-connections/
-  sudo chmod 0600 ~/m2/etc/NetworkManager/system-connections/*
+  sudo rm $prefix/etc/systemd/system/multi-user.target.wants/userconfig.service
+  sudo rm $prefix/etc/systemd/system/getty.target.wants/getty@tty1.service
+
+  sudo cp system-connections/* $prefix/etc/NetworkManager/system-connections/
+  sudo chmod 0600 $prefix/etc/NetworkManager/system-connections/*
 
   sudo tar -xf ../../AdGuardHome_linux_arm64.tar.gz --directory=$HOME/m2/var/
-  sudo cp AdGuardHome.yaml  ~/m2/var/AdGuardHome/
-  sudo cp run_adguard.sh  ~/m2/var/AdGuardHome/
-  sudo chown -R nobody:nogroup ~/m2/var/AdGuardHome/
+  sudo cp AdGuardHome.yaml  $prefix/var/AdGuardHome/
+  sudo cp run_adguard.sh  $prefix/var/AdGuardHome/
+  sudo chown -R nobody:nogroup $prefix/var/AdGuardHome/
 
-  sudo cp -rf ../../adguard ~/m2/var/
-  sudo chmod 777 ~/m2/var/adguard
-  sudo chmod 666 ~/m2/var/adguard/*
+  sudo cp -rf ../../adguard $prefix/var/
+  sudo chmod 777 $prefix/var/adguard
+  sudo chmod 666 $prefix/var/adguard/*
 
-  sudo cp dante-server_1.4.2+dfsg-7+b2_arm64.deb ~/m2/var/cache/apt/archives/
-else
-  sudo touch ~/m2/var/harden_only
+  sudo cp dante-server_1.4.2+dfsg-7+b2_arm64.deb $prefix/var/cache/apt/archives/
 fi
 
-sudo cp -r boot/* ~/m1/
-sudo cp 99-fbturbo.conf ~/m2/usr/share/X11/xorg.conf.d/
-sudo cp 99-calibration.conf ~/m2/usr/share/X11/xorg.conf.d/
+sudo cp 99-fbturbo.conf $prefix/usr/share/X11/xorg.conf.d/
+sudo cp 99-calibration.conf $prefix/usr/share/X11/xorg.conf.d/
 
-sudocpcontent ./modules ~/m2/etc/
-sudocpcontent ./hosts ~/m2/etc/
-sudocpcontent ./sysctl.conf ~/m2/etc/
-sudo chmod -x  ~/m2/etc/*.conf
-sudocpcontent ./00-firewall.conf ~/m2/etc/rsyslog.d/
-sudocpcontent ./dnsmasq.conf ~/m2/etc/NetworkManager/dnsmasq.d/dnsmasq.conf
-sudo chmod -x  ~/m2/etc/NetworkManager/dnsmasq.d/dnsmasq.conf
-sudo ln -s /etc/NetworkManager/dnsmasq.d/dnsmasq.conf ~/m2/etc/NetworkManager/dnsmasq-shared.d/dnsmasq.conf
-sudocpcontent ./NetworkManager.conf ~/m2/etc/NetworkManager/NetworkManager.conf
-sudo cp firewall ~/m2/etc/init.d/
-sudocpcontent ./timesyncd.conf ~/m2/etc/systemd/
-sudo chmod -x ~/m2/etc/systemd/timesyncd.conf
+sudocpcontent ./hosts $prefix/etc/
+sudocpcontent ./sysctl.conf $prefix/etc/
+sudocpcontent ./nftables.conf $prefix/etc/
+sudo chmod -x  $prefix/etc/*.conf
+sudocpcontent ./dnsmasq.conf $prefix/etc/NetworkManager/dnsmasq.d/dnsmasq.conf
+sudo chmod -x  $prefix/etc/NetworkManager/dnsmasq.d/dnsmasq.conf
+sudo ln -s /etc/NetworkManager/dnsmasq.d/dnsmasq.conf $prefix/etc/NetworkManager/dnsmasq-shared.d/dnsmasq.conf
+sudocpcontent ./NetworkManager.conf $prefix/etc/NetworkManager/NetworkManager.conf
+sudo cp firewall $prefix/etc/init.d/
+sudocpcontent ./timesyncd.conf $prefix/etc/systemd/
+sudo chmod -x $prefix/etc/systemd/timesyncd.conf
 
 
-#sudosedi "s|http://archive.raspberrypi.org/debian/ bullseye main |[arch=arm64] https://mirror.init7.net/raspbian/raspbian/ bullseye main arm64|g" ~/m2/etc/apt/sources.list.d/raspi.list
-sudosedi "s|http://archive.raspberrypi.org/|https://archive.raspberrypi.org/|g" ~/m2/etc/apt/sources.list.d/raspi.list
-#sudosedi "s|deb http://deb.debian.org|#deb http://deb.debian.org|g" ~/m2/etc/apt/sources.list
-sudosedi "" -e "s|http://deb.debian.org/debian|https://mirror.init7.net/debian|g" ~/m2/etc/apt/sources.list
-sudosedi "s|http://security.debian.org/|https://security.debian.org/|g" ~/m2/etc/apt/sources.list
+#sudosedi "s|http://archive.raspberrypi.org/debian/ bullseye main |[arch=arm64] https://mirror.init7.net/raspbian/raspbian/ bullseye main arm64|g" $prefix/etc/apt/sources.list.d/raspi.list
+sudosedi "s|http://archive.raspberrypi.org/|https://archive.raspberrypi.org/|g" $prefix/etc/apt/sources.list.d/raspi.list
+#sudosedi "s|deb http://deb.debian.org|#deb http://deb.debian.org|g" $prefix/etc/apt/sources.list
+sudosedi "" -e "s|http://deb.debian.org/debian|https://mirror.init7.net/debian|g" $prefix/etc/apt/sources.list
+sudosedi "s|http://security.debian.org/|https://security.debian.org/|g" $prefix/etc/apt/sources.list
 
-sudocpcontent ./before-network.service ~/m2/usr/lib/systemd/system/
-systemctl --root=~/m2 enable before-network.service
-sudo ln -s /lib/systemd/system/before-network.service ~/m2/etc/systemd/system/multi-user.target.wants/before-network.service
-sudo ln -s /lib/systemd/system/NetworkManager.service ~/m2/etc/systemd/system/multi-user.target.wants/NetworkManager.service
-sudo ln -s /lib/systemd/system/NetworkManager-wait-online.service ~/m2/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
-sudo ln -s /lib/systemd/system/NetworkManager-dispatcher.service ~/m2/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
+sudocpcontent ./before-network.service $prefix/usr/lib/systemd/system/
+systemctl --root=$prefix enable before-network.service
+sudo ln -s /lib/systemd/system/before-network.service $prefix/etc/systemd/system/multi-user.target.wants/before-network.service
+sudo ln -s /lib/systemd/system/nftables.service $prefix/etc/systemd/system/sysinit/nftables.service
+sudo ln -s /lib/systemd/system/NetworkManager.service $prefix/etc/systemd/system/multi-user.target.wants/NetworkManager.service
+sudo ln -s /lib/systemd/system/NetworkManager-wait-online.service $prefix/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
+sudo ln -s /lib/systemd/system/NetworkManager-dispatcher.service $prefix/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
 
-sudo rm ~/m2/etc/systemd/system/multi-user.target.wants/dhcpcd.service
-sudo rm ~/m2/etc/systemd/system/dbus-org.freedesktop.Avahi.service
+sudo rm $prefix/etc/systemd/system/multi-user.target.wants/dhcpcd.service
+sudo rm $prefix/etc/systemd/system/dbus-org.freedesktop.Avahi.service
 
-sudo cp 02-my-dispatcher ~/m2/etc/NetworkManager/dispatcher.d/
-sudo ln -s /etc/NetworkManager/dispatcher.d/02-my-dispatcher ~/m2/etc/NetworkManager/dispatcher.d/pre-up.d/02-my-dispatcher
-sudo ln -s /etc/NetworkManager/dispatcher.d/02-my-dispatcher ~/m2/etc/NetworkManager/dispatcher.d/pre-down.d/02-my-dispatcher
-sudo chmod 0755 ~/m2/etc/NetworkManager/dispatcher.d/02-my-dispatcher
+sudo cp 02-hood-dispatcher $prefix/etc/NetworkManager/dispatcher.d/
+sudo ln -s /etc/NetworkManager/dispatcher.d/02-hood-dispatcher $prefix/etc/NetworkManager/dispatcher.d/pre-up.d/02-hood-dispatcher
+sudo ln -s /etc/NetworkManager/dispatcher.d/02-hood-dispatcher $prefix/etc/NetworkManager/dispatcher.d/pre-down.d/02-hood-dispatcher
+sudo chmod 0755 $prefix/etc/NetworkManager/dispatcher.d/02-hood-dispatcher
 
-sudo ln -s /etc/NetworkManager/dnsmasq.d/dnsmasq.conf ~/m2/etc/NetworkManager/dnsmasq-shared.d/dnsmasq.conf
-sudo cp update_eeprom ~/m2/
-sudo chmod +x ~/m2/update_eeprom
-sudo ln -s /update_eeprom ~/m2/run_once
-#sudo touch ~/m2/do_init
+sudo ln -s /etc/NetworkManager/dnsmasq.d/dnsmasq.conf $prefix/etc/NetworkManager/dnsmasq-shared.d/dnsmasq.conf
+sudo cp update_eeprom $prefix/
+sudo chmod +x $prefix/update_eeprom
+sudo ln -s /update_eeprom $prefix/run_once
+#sudo touch $prefix/do_init
--- scripts/mntinstall.sh
+++ scripts/mntinstall.sh
@@ -1,15 +1,18 @@
-mkdir ~/m1 ~/m2
+mkdir ~/hood_install
 
 machine=$(uname -s)
 
 if [ $machine = "FreeBSD" ]; then
-  sudo mount -t msdos /dev/da1s1 ~/m1
-  sudo lklfuse -o allow_other,type=ext4 /dev/da1s2 ~/m2
+  sudo lklfuse -o allow_other,type=ext4 /dev/da1s2 ~/hood_install
+  sudo mkdir -p ~/hood_install/boot/firmware
+  sudo mount -t msdos /dev/da1s1 ~/hood_install/boot/firmware
 elif [ $machine = "Linux" ]; then
-  sudo mount /dev/sdb1 ~/m1
-  sudo mount /dev/sdb2 ~/m2
+  sudo mount /dev/sdb2 ~/hood_install
+  sudo mkdir -p ~/hood_install/boot/firmware
+  sudo mount /dev/sdb1 ~/hood_install/boot/firmware
 fi
 
 
-./install.sh "$@"
-sudo umount ~/m1 ~/m2
+./install.sh "$@" prefix=~/hood_install/
+sudo umount ~/hood_install/boot/firmware
+sudo umount ~/hood_install
