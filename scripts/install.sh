69ed690399257f5e0658fd69bd57dc3f89f745d3
Yong Bin <yongb@usi.ch>
2023-11-03 03:27:24 +0200

    scripts: trust Baltimore_CyberTrust to accept lets encrypt

--- scripts/install.sh
+++ scripts/install.sh
@@ -68,7 +68,7 @@ sudocpcontent ./nftables.conf $prefix/etc/
 
 if grep "#" $prefix/etc/ca-certificates.conf; then 
   sed -e '/^$/d' -e '/^#/d' $prefix/etc/ca-certificates.conf | xargs -I {} sh -c "openssl x509 -text -in ${prefx}/usr/share/ca-certificates/{}|grep \"C = US,\">/dev/null&&echo {}||echo \!{}" > /tmp/hood-install/ca-certificates.conf
-  sudosedi "s/^!(.*(Comodo|GlobalSign).*)/\1/ig" /tmp/hood-install/ca-certificates.conf
+  sudosedi "s/^!(.*(Comodo|GlobalSign|Baltimore_CyberTrust).*)/\1/ig" /tmp/hood-install/ca-certificates.conf
   sudosedi "s/^[^\!].*(AffirmTrust|Trustwave).*/\!\0/ig" /tmp/hood-install/ca-certificates.conf
   
   sudocpcontent /tmp/hood-install/ca-certificates.conf $prefix/etc/ca-certificates.conf
