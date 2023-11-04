496473b3bd2763a6bb8cab728b649c34031fb48c
Yong Bin <yongb@usi.ch>
2023-11-04 02:18:46 +0200

    code.sh: add support to codium on linux

--- code.sh
+++ code.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 env_dir=$(readlink -f $(dirname "$0"))/environment
 mkdir -p "$env_dir/code/extensions" "$env_dir/code/data"
-/usr/local/share/code-oss/code-oss --extensions-dir "$env_dir/code/extensions" --disable-gpu --ms-enable-electron-run-as-node .
+executable=/usr/share/codium/codium
+if test -f /usr/local/share/code-oss/code-oss; then
+  executable=/usr/local/share/code-oss/code-oss
+fi
+$executable --extensions-dir "$env_dir/code/extensions" --disable-gpu --ms-enable-electron-run-as-node .
 # --user-data-dir "$env_dir/code/data"
