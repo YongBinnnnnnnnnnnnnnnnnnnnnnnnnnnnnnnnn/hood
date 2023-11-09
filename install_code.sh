244316b10529b4492a99d09c2c83a41322b8188b
Yong Bin <yongb@usi.ch>
2023-11-09 16:09:23 +0000

    add install_code.sh

---
+++ install_code.sh
@@ -0,0 +1,4 @@
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/vscodium-archive-keyring.gpg | sha256sum
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt-get update
+sudo apt-get install -y codium
