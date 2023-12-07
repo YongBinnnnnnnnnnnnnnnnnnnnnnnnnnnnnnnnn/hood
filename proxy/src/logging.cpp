5e8e822e961239e4d9aa20e13574406dbeb7adc5
Yong Bin <yongb@usi.ch>
2023-12-07 00:28:45 +0000

    proxy: mitigate the gcc bug

--- proxy/src/logging.cpp
+++ proxy/src/logging.cpp
@@ -1,26 +1,38 @@
-#include "logging.hpp"
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/utility/setup/console.hpp>
-
 #include <boost/log/expressions.hpp>
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <iostream>
+
+#include "logging.hpp"
 namespace hood_proxy {
 
 namespace expr = boost::log::expressions;
+
+#ifndef __GNUC__
+#define BUGGY_GCC 0
+#else
+#define BUGGY_GCC \
+  (__GNUC__ == 12 && __GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ == 0)
+#endif
+
+#if !BUGGY_GCC
 
 const auto date_time_formatter =
     expr::stream << expr::format_date_time<boost::posix_time::ptime>(
                         "TimeStamp", "\e[32m%m%d %H:%M:%S.%f")
                  << expr::message;
+#endif
 
+const auto simple_formatter = expr::stream << expr::message;
 void InitLogging() {
   boost::log::add_common_attributes();
+#if BUGGY_GCC
+  boost::log::add_console_log();
+#else
   boost::log::add_console_log()->set_formatter(date_time_formatter);
-  ;
+#endif
 }
 
 }  // namespace hood_proxy
