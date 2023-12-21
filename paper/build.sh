96adfc7ad5b77dc2394a1ed87cde715336e2a94e
Yong Bin <yongb@usi.ch>
2023-12-21 00:49:57 +0000

    paper: add logical view of actor

--- paper/build.sh
+++ paper/build.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-pdflatex paper.tex
+if ! test -f ~/Downloads/plantuml.jar; then
+  curl -L -C - "https://github.com/plantuml/plantuml/releases/download/v1.2023.13/plantuml-1.2023.13.jar" -o ~/Downloads/plantuml.jar
+fi
+
+if ! apt list --installed graphviz | grep -q installed; then
+  sudo apt install graphviz -y
+fi
+
+java -jar ~/Downloads/plantuml.jar -tpng graphics/puml/*.puml
+latexmk -synctex=1 -file-line-error -pdf  paper.tex
+#-interaction=nonstopmode 
