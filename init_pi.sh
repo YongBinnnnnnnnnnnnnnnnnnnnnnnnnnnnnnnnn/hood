f1575599bd008a1c0dd51ffaa540f9963918ac4e
Yong Bin <yongb@usi.ch>
2023-12-31 22:08:43 +0000

    paper: refine intro

--- init_pi.sh
+++ init_pi.sh
@@ -4,7 +4,8 @@ echo $script_dir
 git config --global user.name "Yong Bin"
 git config --global user.email yongb@usi.ch
 
-sudo apt -y autoremove --purge bluez bluez-firmware avahi-daemon cups
+sudo apt -y autoremove --purge bluez bluez-firmware avahi-daemon
+# cups
 if curl 1.1.1.1; then
   sudo apt -y install firefox chromium-browser vlc linux-compiler-gcc-12-arm --upgrade 
   sudo apt -y install texlive-font-utils texlive latexmk texlive-fonts-extra texlive-extra-utils
