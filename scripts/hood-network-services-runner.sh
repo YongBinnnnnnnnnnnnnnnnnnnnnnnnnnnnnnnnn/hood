ec4f396b73f54a4a8c1147230e0c8d4cec4c84da
Yong Bin <yongb@usi.ch>
2024-01-04 21:36:01 +0000

    hood-network-service: better log

--- scripts/hood-network-services-runner.sh
+++ scripts/hood-network-services-runner.sh
@@ -12,6 +12,6 @@ if [ "$1" = "lo" ]||[ "$1" = "" ]; then
   sudo -u nobody -g nogroup /bin/sh -c "ulimit -S -n 1000000;ulimit -S -s 819200;/usr/local/lib/hood/hood-tls-proxy --config=/etc/hood_proxy.conf" >> /var/log/hood-tls-proxy.log 2>&1 &
   
   tail -f /var/log/hood-name-service.log |grep --line-buffered "\->" |sed "s/.*\-> b/DNS query /" > $log_device &
-  tail -f /var/log/hood-tls-proxy.log |grep verifying|sed "s/.* verify/verify/" > $log_device &
+  tail -f /var/log/hood-tls-proxy.log |grep --line-buffered "verifying\|DoConnectHost"|sed -e "s/^[^.]*\.cpp:[0-9]* operator...  //" -e "s/:443 :/ :/" -e "s/^[^.]*\.cpp:[0-9]* DoConnectHost\: //" > $log_device &
   /usr/sbin/dnsmasq --keep-in-foreground --no-hosts --no-resolv --no-dhcp --interface=lo --local-service --listen-address=127.0.0.1 --bind-interfaces --no-poll --address=/#/127.0.0.1 --cache-size=0 
 fi
