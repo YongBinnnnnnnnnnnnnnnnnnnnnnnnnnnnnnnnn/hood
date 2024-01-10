1fe443401f51d1a981e3aef81a6cc608edd96149
Bin Yong <yongb@usi.ch>
2024-01-10 20:57:00 +0100

    Create Collect Apple security releases-2024-01-10.user.js

---
+++ paper/data/apple_security_releases/Collect Apple security releases-2024-01-10.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Collect Apple security releases
+// @namespace    https://github.com/YongBinnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn/hood/
+// @version      2024-01-10
+// @description  I hate surveillance.
+// @author       Bin Yong
+// @match        https://support.apple.com/en-us/HT201222
+// @grant        GM_getResourceText
+// ==/UserScript==
+
+(async function() {
+  'use strict';
+  const links = document.querySelectorAll("#tableWraper a");
+  const target_products = [
+    "watchOS",
+    "iOS",
+    "iPadOS",
+    "macOS",
+    "tvOS",
+  ];
+  const renderer = document.querySelector("div.footer-wrapper");
+  unsafeWindow.yong = {};
+  for (const link of links) {
+    let is_target_product = false;
+    const name = link.textContent;
+    for (const product of target_products) {
+      if (name.indexOf(product) != -1) {
+        is_target_product = true;
+      }
+    }
+    if (!is_target_product) {
+      continue;
+    }
+    total += 1;
+    console.log(name);
+    console.log(link.href);
+    const response = await fetch(link.href);
+    let update_text = await response.text();
+    if (update_text.indexOf('<section class="section section-content">') != -1) {
+      update_text ='<section class="section section-content">' +  update_text.split('<section class="section section-content">')[1];
+      update_text = update_text.split('</section>')[0] + '</section>';
+    }
+    else {
+      update_text ='<div id="sections">' +  update_text.split('<div id="sections">')[1];
+      update_text = update_text.split("<div id='helpful-rating-wrapper'>")[0];
+      update_text = update_text.substr(0, update_text.lastIndexOf("</div>"));
+    }
+    renderer.innerHTML = update_text;
+    update_text = renderer.innerText;
+    update_text = update_text.replace(/\s\s+/gm, "");
+    //update_text = update_text.replace(/\n\n/gm, "\n");
+    //console.log(renderer.innerText);
+    unsafeWindow.yong[name] = update_text;
+  }
+  document.body.innerText = JSON.stringify(unsafeWindow.yong);
+})();
