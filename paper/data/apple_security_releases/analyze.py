d30764423b3b89d9d378f676be16159381f78104
Bin Yong <yongb@usi.ch>
2024-01-13 18:39:18 +0100

    Update analyze.py

--- paper/data/apple_security_releases/analyze.py
+++ paper/data/apple_security_releases/analyze.py
@@ -10,7 +10,7 @@ statastics = {
 }
 for k, v in data.items():
   #print(text_string)
-  match_pattern = re.findall(r'\n[a-z\-A-Z]{2,15}\n', v)
+  match_pattern = re.findall(r'\n[ a-z\-A-Z]{2,15}\n', v)
   for word in match_pattern:
     word = word.strip()
     count = statastics.get(word,0)
@@ -28,7 +28,7 @@ def draw_bar(word, value, max):
   print(word + ": |" + "█"*length + " "*(bar_width - length) + "| " + str(value)  )
 
 max_count = max(statastics.values())
-print("Word" + " "*(word_width - 4) + ":" + " "*(bar_width+2) + "| Number of times" )
+print("Word" + " "*(word_width - 4) + " " + " "*(bar_width+2) + "  Number of times" )
 draw_count = 20
 for k, v in statastics.copy().items():
   draw_bar(k, v, max_count)
