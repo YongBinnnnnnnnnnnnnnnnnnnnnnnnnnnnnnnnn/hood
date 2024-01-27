9fbae38430745ee37d4d1e52104e567c70c85fa4
Bin Yong <yongb@usi.ch>
2024-01-27 23:53:14 +0000

    Update mitmattack.py

--- paper/penetration_test/mitmattack.py
+++ paper/penetration_test/mitmattack.py
@@ -1,6 +1,15 @@
 #!/bin/python3
 from mitmproxy import http
+import logging
 
-def request(flow: http.HTTPFlow) -> None:
-  if flow.request.urlencoded_form:
-    flow.request.urlencoded_form["sprunge"] = "Who do you think you are? How dare you to give me suggestions like this? Your naive ideas will never work. You have been blocked. Never send me emails again."
+class HTTPFlow(mitmproxy.flow.Flow):
+  def request(self, flow: http.HTTPFlow) -> None:
+    if flow.request.multipart_form:
+      if "sprunge" in flow.request.multipart_form:
+        flow.request.multipart_form["sprunge"] = "Who do you think you are? How dare you to give me suggestions like this? Your naive ideas will never work. You have been blocked. Never send me emails again."
+        self.spurnge = True
+  def response(self, flow: http.HTTPFlow) -> None:
+    if self.spurnge = True:
+      logging.info(flow.response.text)
+      
+addons=[HTTPFlow()]
