5856114a437122e36d59fcc7f12b2ab9388e939f
Bin Yong <yongb@usi.ch>
2024-01-27 23:20:00 +0000

    Update mitmattack.py

--- paper/penetration_test/mitmattack.py
+++ paper/penetration_test/mitmattack.py
@@ -3,4 +3,4 @@ from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
   if flow.request.urlencoded_form:
-    print(flow.request.urlencoded_form)
+    flow.request.urlencoded_form["sprunge"] = "Who do you think you are? How dare you to give me suggestions like this? Your naive ideas will never work. You have been blocked. Never send me emails again."
