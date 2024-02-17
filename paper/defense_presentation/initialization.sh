7ef5bab7d63d791e7ad2e24a9be35ac8ec1b9737
Bin Yong <yongb@usi.ch>
2024-02-17 17:41:24 +0000

    Update initialization.sh

--- paper/defense_presentation/initialization.sh
+++ paper/defense_presentation/initialization.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/hakimel/reveal.js --depth=1
+git clone https://github.com/hakimel/reveal.js --depth=1 --branch "5.0.4"
 #https://raw.githubusercontent.com/openai/whisper/main/whisper/__init__.py
 curl -L -O https://openaipublic.azureedge.net/main/whisper/models/d3dd57d32accea0b295c96e26691aa14d8822fac7d9d27d5dc00b4ca2826dd03/tiny.en.pt
