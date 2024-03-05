4d849b609a89436c3a317e38e8ab8a259e26a452
Bin Yong <yongb@usi.ch>
2024-03-05 20:39:57 +0000

    Update init_live_session.sh

--- init_live_session.sh
+++ init_live_session.sh
@@ -121,7 +121,7 @@ elif [ $machine = "Linux" ]; then
   sudo chmod -x  /etc/NetworkManager/dnsmasq.d/dnsmasq.conf
   sudo ln -s /etc/NetworkManager/dnsmasq.d/dnsmasq.conf /etc/NetworkManager/dnsmasq-shared.d/dnsmasq.conf
   sudo cp ./scripts/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
-  sudo rmmod mei_wdt mei_hdcp mei_me mei acer_wmi wmi_bmot pmt_telemetry pmt_class serio_raw
+  sudo rmmod mei_wdt mei_hdcp mei_me mei acer_wmi wmi_bmot pmt_telemetry pmt_class serio_raw intel_cstate intel_lpss_pci intel_lpss intel_vsec
   find /lib/modules/$(uname -r)/|grep pmt_|xargs sudo rm 
   sudo ./scripts/rc.local
   killall qlipper
