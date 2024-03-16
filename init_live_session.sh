acb7014e2da13eb5ea4d121d5c54a66b6b5acb69
Bin Yong <yongb@usi.ch>
2024-03-16 00:21:23 +0000

    dummy

--- init_live_session.sh
+++ init_live_session.sh
@@ -112,9 +112,12 @@ if sudo -E pkg upgrade -y firefox; then
   sudo -E pkg -o INSTALL_AS_USER=true install -y tex-dvipsk
 fi
 elif [ $machine = "Linux" ]; then
-  # failed concept
+  # failed due to kernel lockdownn
   # lspci|grep hunderbolt | grep "PCI\|NHI" |cut -d ' ' -f 1|xargs -L 1 -I {} sudo setpci -s {} CAP_PM+4.b=0b
   #lspci -D |grep hunderbolt | grep "PCI\|NHI" |sed -E "s|(\w+:\w+)([^ ]+).*|\1/\1\2|"|xargs -L 1 -I {} sh -c "echo 1 | sudo tee /sys/devices/pci{}/remove"
+  #lspci -D |grep hunderbolt | grep "NHI" |cut -d " " -f 1|xargs -L 1 -I {} sh -c "echo {} | sudo tee /sys/bus/pci/drivers/thunderbolt/unbind"
+  #lspci -D |grep hunderbolt | grep "PCI" |cut -d " " -f 1|xargs -L 1 -I {} sh -c "echo {} | sudo tee /sys/bus/pci/drivers/pcieport/unbind"
+  #lspci -D |grep hunderbolt | grep "PCI\|NHI" |sed -E "s|(\w+:\w+)([^ ]+).*|\1/\1\2|"|xargs -I "{}" cat /sys/devices/pci{}/vendor /sys/devices/pci{}/device|xargs -L 2 echo|sudo tee /sys/bus/pci/drivers/vfio-pci/new_id
   boltctl config global.auth-mode disabled
   sudo systemctl disable avahi-daemon
   sudo systemctl stop NetworkManager ntpd avahi-daemon.socket avahi-daemon cups cups-browsed exim4
