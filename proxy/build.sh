4cab6a3eb4d9e45fdc8bc252e273f59254e29ae8
Bin Yong <yongb@usi.ch>
2024-03-25 22:51:36 +0000

    Update build.sh

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 script_dir=$(readlink -f $(dirname "$0"))
-mkdir -p $script_dir/build/
-cd $script_dir/build/
 #export CC=clang
 #export CXX=clang++
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j4
+
+type=Release
+arch=$(uname -r|cut -d - -f 3)
+system=$(uname -s)
+
+for arg in "$@"; do
+  case $arg in 
+    type=*) type=$(echo $arg|sed "s/[^=]*=//");;
+    arch=*) arch=$(echo $arg|sed "s/[^=]*=//");;
+  esac
+done
+
+mkdir -p $script_dir/build/$type/$arch
+cd $script_dir/build/$type/$arch
+
+cmake -DCMAKE_BUILD_TYPE=$type -DOPENSSL_ROOT_DIR=$script_dir/libs/$system/$arch/openssl -DBOOST_ROOT=$script_dir/libs/$system/$arch/boost ..
+make -j $(nproc --all)
